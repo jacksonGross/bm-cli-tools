30038814629b624da828a6ace17c0d35333bd0ef
jacksonGross <jackson_gross@hotmail.com>
2016-08-26 16:51:31 +1000

    inital commit

---
+++ spec/bm-cli-tools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BmCliToolsView from '../lib/bm-cli-tools-view';
+
+describe('BmCliToolsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
